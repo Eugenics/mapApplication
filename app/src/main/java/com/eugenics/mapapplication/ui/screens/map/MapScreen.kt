3f0b46caac8fd045ac74f7ce55cf65210236ce87
Podzorov Eugene <p.eugenics@gmail.com>
2022-06-24 15:48:37 +0700

    added map markers

--- app/src/main/java/com/eugenics/mapapplication/ui/screens/map/MapScreen.kt
+++ app/src/main/java/com/eugenics/mapapplication/ui/screens/map/MapScreen.kt
@@ -3,9 +3,9 @@ package com.eugenics.mapapplication.ui.screens.map
 import android.content.Context
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidViewBinding
@@ -19,8 +19,11 @@ import com.tomtom.sdk.location.android.AndroidLocationEngine
 import com.tomtom.sdk.location.android.AndroidLocationEngineConfig
 import com.tomtom.sdk.maps.display.MapOptions
 import com.tomtom.sdk.maps.display.camera.CameraOptions
+import com.tomtom.sdk.maps.display.image.ImageFactory
 import com.tomtom.sdk.maps.display.location.LocationMarkerOptions
 import com.tomtom.sdk.maps.display.location.LocationMarkerType
+import com.tomtom.sdk.maps.display.marker.Marker
+import com.tomtom.sdk.maps.display.marker.MarkerOptions
 import com.tomtom.sdk.maps.display.ui.MapFragment
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -44,9 +47,20 @@ private fun Map(
     context: Context = LocalContext.current,
     supportFragmentManager: FragmentManager
 ) {
+    val markers = rememberSaveable { mutableListOf<MarkerOptions>() }
+    val showMarkerDialog = remember { mutableStateOf(false) }
+    val balloonText = remember { mutableStateOf("") }
+
+    if (showMarkerDialog.value) {
+        MarkerDialog(
+            balloonText = balloonText.value,
+            onDismissAction = { showMarkerDialog.value = false }
+        )
+    }
+
     val androidLocationEngineConfig = AndroidLocationEngineConfig(
-        minTimeInterval = Duration.ofMilliseconds(250L),
-        minDistance = Distance.ofMeters(20.0)
+        minTimeInterval = Duration.ofMilliseconds(1000L),
+        minDistance = Distance.ofMeters(10.0)
     )
     val locationEngine: LocationEngine = AndroidLocationEngine(
         context = context,
@@ -55,29 +69,99 @@ private fun Map(
         it.enable()
     }
 
-    val cameraOptions = CameraOptions(
-        position = locationEngine.lastKnownLocation?.position,
-        zoom = 16.0,
-        tilt = 0.0
-    )
-    val mapOptions = MapOptions(
-        mapKey = BuildConfig.MAPS_API_KEY,
-        cameraOptions = cameraOptions
-    )
-    val mapFragment = MapFragment.newInstance(mapOptions = mapOptions)
+    val cameraOptions = rememberSaveable {
+        mutableListOf(
+            CameraOptions(
+                position = locationEngine.lastKnownLocation?.position,
+                zoom = 15.0,
+                tilt = 0.0
+            )
+        )
+    }
+
+    val mapOptions = remember {
+        MapOptions(
+            mapKey = BuildConfig.MAPS_API_KEY,
+            cameraOptions = cameraOptions.first()
+        )
+    }
 
     AndroidViewBinding(
         factory = MapFrameViewBinding::inflate,
         modifier = Modifier.padding(paddingValues = paddingValues),
     ) {
-        mapFragment.getMapAsync { map ->
-            locationEngine.disable()
-            map.setLocationEngine(locationEngine)
-            map.enableLocationMarker(LocationMarkerOptions(LocationMarkerType.CHEVRON))
-            locationEngine.enable()
-        }
-        supportFragmentManager.beginTransaction()
-            .replace(this.mapContainer.id, mapFragment)
-            .commit()
+        val mapFragment =
+            if (supportFragmentManager.findFragmentByTag("MAP_TAG")?.isAdded != true) {
+                val fragment = MapFragment.newInstance(mapOptions = mapOptions)
+
+                supportFragmentManager.beginTransaction()
+                    .replace(this.mapContainer.id, fragment, "MAP_TAG")
+                    .commit()
+
+                fragment
+            } else {
+                supportFragmentManager.findFragmentByTag("MAP_TAG") as MapFragment
+            }.also {
+                it.getMapAsync { map ->
+                    locationEngine.disable()
+                    map.setLocationEngine(locationEngine)
+                    map.enableLocationMarker(LocationMarkerOptions(LocationMarkerType.CHEVRON))
+                    locationEngine.enable()
+
+                    map.isMarkersFadingEnabled = false
+                    map.isMarkersShrinkingEnabled = false
+
+                    if (markers.isNotEmpty()) {
+                        markers.forEach {
+                            map.addMarker(it)
+                        }
+                    }
+
+                    map.addOnMapLongClickListener { coordinate ->
+                        val markerOptions = MarkerOptions(
+                            coordinate = coordinate,
+                            balloonText = "Marker: ${coordinate.longitude.toString()},${coordinate.latitude.toString()}",
+                            pinImage = ImageFactory.fromAssets("icons/location_96.png")
+                        )
+                        markers.add(markerOptions)
+                        map.addMarker(markerOptions)
+                        true
+                    }
+                    map.addOnMarkerClickListener { marker: Marker ->
+                        if (!marker.isSelected()) {
+                            balloonText.value = marker.balloonText
+                            showMarkerDialog.value = true
+                        }
+                    }
+
+                    map.addOnCameraChangeListener {
+                        cameraOptions.add(
+                            0, CameraOptions(
+                                position = map.cameraPosition().position,
+                                zoom = map.cameraPosition().zoom,
+                                tilt = map.cameraPosition().tilt
+                            )
+                        )
+                    }
+                }
+            }
     }
+}
+
+@Composable
+private fun MarkerDialog(
+    balloonText: String,
+    onDismissAction: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = onDismissAction,
+        dismissButton = {},
+        confirmButton = {},
+        title = {
+            Text("Marker", style = MaterialTheme.typography.titleSmall)
+        },
+        text = {
+            Text(text = balloonText, style = MaterialTheme.typography.bodyMedium)
+        }
+    )
 }
