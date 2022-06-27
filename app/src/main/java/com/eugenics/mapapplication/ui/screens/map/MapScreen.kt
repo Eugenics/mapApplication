9bda9a10f396cf9bc5181f02ceb6c89822ff0331
Podzorov Eugene <p.eugenics@gmail.com>
2022-06-27 19:06:40 +0700

    request permissions

--- app/src/main/java/com/eugenics/mapapplication/ui/screens/map/MapScreen.kt
+++ app/src/main/java/com/eugenics/mapapplication/ui/screens/map/MapScreen.kt
@@ -1,7 +1,10 @@
 package com.eugenics.mapapplication.ui.screens.map
 
+import android.Manifest
 import android.content.Context
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -12,6 +15,10 @@ import androidx.compose.ui.viewinterop.AndroidViewBinding
 import androidx.fragment.app.FragmentManager
 import com.eugenics.mapapplication.BuildConfig
 import com.eugenics.mapapplication.databinding.MapFrameViewBinding
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionStatus
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.shouldShowRationale
 import com.tomtom.sdk.common.measures.Distance
 import com.tomtom.sdk.common.measures.Units
 import com.tomtom.sdk.common.time.Duration
@@ -28,18 +35,34 @@ import com.tomtom.sdk.maps.display.marker.MarkerOptions
 import com.tomtom.sdk.maps.display.ui.MapFragment
 import com.tomtom.sdk.maps.display.ui.scale.ScaleView
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
 @Composable
 fun MapScreen(
     context: Context,
     fragmentManager: FragmentManager
 ) {
+    val locationPermissionState = rememberPermissionState(Manifest.permission.ACCESS_FINE_LOCATION)
     Scaffold { paddingValues ->
-        Map(
-            paddingValues = paddingValues,
-            supportFragmentManager = fragmentManager,
-            context = context
-        )
+        if (locationPermissionState.status == PermissionStatus.Granted) {
+            Map(
+                paddingValues = paddingValues,
+                supportFragmentManager = fragmentManager,
+                context = context
+            )
+        } else {
+            Column {
+                val textToShow = if (locationPermissionState.status.shouldShowRationale) {
+                    "The location data is important for this app. Please grant the permission."
+                } else {
+                    "Location permission required for this feature to be available. " +
+                            "Please grant the permission"
+                }
+                Text(textToShow)
+                Button(onClick = { locationPermissionState.launchPermissionRequest() }) {
+                    Text("Request permission")
+                }
+            }
+        }
     }
 }
 
@@ -145,7 +168,6 @@ private fun Map(
                             )
                         )
                     }
-
                     it.scaleView.units = Units.METRIC
                 }
             }
